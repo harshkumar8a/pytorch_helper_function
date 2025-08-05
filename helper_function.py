16a67cf4f5f8796d83c0844d88ab06a50c982bae
HARSH KUMAR <121298134+harshkumar8a@users.noreply.github.com>
2025-08-05 13:11:18 +0530

    Update helper_function.py

--- helper_function.py
+++ helper_function.py
@@ -2,28 +2,47 @@ import requests
 import zipfile
 from pathlib import Path
 
-def getDatasetsUsingLink(
-  image_path_data, 
-  url
-):
-  # Setup path to data folder
-  data_path = Path("data/")
-  image_path = data_path / "{image_path_data}"
-  
-  # If the image folder doesn't exist, download it and prepare it... 
-  if image_path.is_dir():
-      print(f"{image_path} directory exists.")
-  else:
-      print(f"Did not find {image_path} directory, creating one...")
-      image_path.mkdir(parents=True, exist_ok=True)
-      
-      # Download data
-      with open(data_path / "{image_path_data}.zip", "wb") as f:
-          request = requests.get("{url}")
-          print("Downloading datasets....")
-          f.write(request.content)
-  
-      # Unzip data
-      with zipfile.ZipFile(data_path / "{image_path_data}.zip", "r") as zip_ref:
-          print("Unzipping datasets...") 
-          zip_ref.extractall(image_path)
+def download_data(source: str, 
+                  destination: str,
+                  remove_source: bool = True) -> Path:
+    """Downloads a zipped dataset from source and unzips to destination.
+
+    Args:
+        source (str): A link to a zipped file containing data.
+        destination (str): A target directory to unzip data to.
+        remove_source (bool): Whether to remove the source after downloading and extracting.
+    
+    Returns:
+        pathlib.Path to downloaded data.
+    
+    Example usage:
+        download_data(source="https://github.com/..............................")
+    """
+    # Setup path to data folder
+    data_path = Path("data/")
+    image_path = data_path / destination
+
+    # If the image folder doesn't exist, download it and prepare it... 
+    if image_path.is_dir():
+        print(f"[INFO] {image_path} directory exists, skipping download.")
+    else:
+        print(f"[INFO] Did not find {image_path} directory, creating one...")
+        image_path.mkdir(parents=True, exist_ok=True)
+        
+        # Download datasets............
+        target_file = Path(source).name
+        with open(data_path / target_file, "wb") as f:
+            request = requests.get(source)
+            print(f"[INFO] Downloading {target_file} from {source}...")
+            f.write(request.content)
+
+        # Unzip datasets..................
+        with zipfile.ZipFile(data_path / target_file, "r") as zip_ref:
+            print(f"[INFO] Unzipping {target_file} data...") 
+            zip_ref.extractall(image_path)
+
+        # Remove .zip file
+        if remove_source:
+            os.remove(data_path / target_file)
+    
+    return image_path
